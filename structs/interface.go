ea4d8bd473c5e99549cb6931b04d58ddedc6aac7
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2018-08-30 12:00:50 -0500

    defer

--- structs/interface.go
+++ structs/interface.go
@@ -1,10 +1,18 @@
 package structs
 
+import "fmt"
+
 type Platzi interface {
 	Subscribe(name string)
 }
 
+func deferTest() {
+	fmt.Println("La Funcion interface ha culminado")
+}
+
 func InterfaceTest() {
+	defer deferTest()
+
 	platziCourse := PlatziCourse{
 		Name:   "Go",
 		Slug:   "go",
